1726d92b5b9cab2fa0b772414aef48cd72829da7
Rene Rösch <r.roesch90@googlemail.com>
2021-02-20 22:50:34 +0100

    finish Barbados, add further sources for Geography Now, update external dependencies

--- app/src/map/countries.js
+++ app/src/map/countries.js
@@ -78,6 +78,7 @@ export default {
                 <ul class="modal__content-list--wordwrapped">
                     <li><a href="https://de.wikipedia.org/wiki/Ungarn">https://de.wikipedia.org/wiki/Ungarn</a></li>
                     <li><a href="https://www.youtube.com/watch?v=2U41I_DtQzQ">https://www.youtube.com/watch?v=2U41I_DtQzQ</a></li>
+                    <li><a href="https://www.youtube.com/watch?v=omx66rFK5yM">https://www.youtube.com/watch?v=omx66rFK5yM</a></li>
                 </ul>
             `;
         }
@@ -132,6 +133,7 @@ export default {
                 <ul class="modal__content-list--wordwrapped">
                     <li><a href="https://de.wikipedia.org/wiki/Salomonen">https://de.wikipedia.org/wiki/Salomonen</a></li>
                     <li><a href="https://www.youtube.com/watch?v=mb3KoX7JT6g">https://www.youtube.com/watch?v=mb3KoX7JT6g</a></li>
+                    <li><a href="https://www.youtube.com/watch?v=8Gl6iy7OEM4">https://www.youtube.com/watch?v=8Gl6iy7OEM4</a></li>
                 </ul>
             `;
         }
@@ -152,18 +154,40 @@ export default {
                 <h3>Geographie & Menschen</h3>
                 <ul>
                     <li>Hauptstadt: Bridgetown</li>
+                    <li>unabhängiger Inselstatt in Inselkette der Kleinen Antillen</li>
+                    <li>Teil des Commonwealth of Nations, besteht aus 19 Parishes (Gemeinden)</li>
+                    <li>früher sehr viel tropischer Regendwald, viele der Flächen für Zuckerrohrplantagen gerodet</li>
+                    <li>über 400 unterschiedliche Strände</li>
+                    <li>Ethnien: 80%  Barbadier, die primär Nachkommen afrikanischer Sklaven sind, einzelne Weiße & Asiaten</li>
+                    <li>Sprache: Bajan, ein auf dem Englischen basierender kreolischer Dialekt</li>
+                    <li>Religion: Anglikaner, Anhänger der Pfingsbewegung, Adventisten und Methodisten</li>
                 </ul>
                 <h3>Kultur</h3>
                 <ul>
+                    <li>wichtigster Export ist nach wie vor Zuckerrohr und Rum</li>
+                    <li>war jahreland Startplatz von Forschungsraketen</li>
+                    <li>unterhält das Barbados Cloud Observatory zusammen mit dem Hamburger Max-Planck-Institut für Meteorologie</li>
+                    <li>Pferdesportarten wie Polo unter der wohlhabenden Bevölkerung beliebt</li>
+                    <li>Fliegende Fische sind Teil typischer Nationalgerichte und Symbole im Alltag</li>
                 </ul>
                 <h3>Geschichte & Politik</h3>
                 <ul>
-                    <li>parlamentarische Monarchie unter Führung von Königin Elisabeth II. und dem eigenen Premierminister</li>
+                    <li>ursprünglich von Arawak und Kariben besiedelt</li>
+                    <li>1536 vom Portugiesen Pedro Campos entdeckt, die Bewohner wurden versklavt</li>
+                    <li>1625 von England übernommen, aufgrund der vielen Fliehenden war die Insel aber nahezu verwaist</li>
+                    <li>1627 wurden Sklaven aus Irland und Indentur-Sklaven von den britischen Inseln angesiedelt</li>
+                    <li>1652 nach Konflikten mit England wurden die Rechte des Parlaments und die der Bürger in der „Charta of Barbados, or Articles of Agreement“ niedergelegt</li>
+                    <li>1838 nach der Emanzipation die anfangs nur für englische Plantagenbesitzer geltenden Rechte wurden für Sklaven ausgeweitet</li>
+                    <li>1966 zur Unabhängigkeit auf die schwarze Bevölkerungsmehrheit ausgedehnt</li>
+                    <li>noch heute eine parlamentarische Monarchie unter Führung von Königin Elisabeth II. und dem eigenen Premierminister</li>
+                    <li>anlässlich der 55. Wiederkehr des Unabhängigkeitstages soll das Land am 30. November 2021 zur Republik erklärt werden</li>
                 </ul>
                 <hr>
                 <h3>Quellen</h3>
                 <ul class="modal__content-list--wordwrapped">
                     <li><a href="https://de.wikipedia.org/wiki/Barbados">https://de.wikipedia.org/wiki/Barbados</a></li>
+                    <li><a href="https://www.youtube.com/watch?v=xZn6MfDJlHk">https://www.youtube.com/watch?v=xZn6MfDJlHk</a></li>
+                    <li><a href="https://www.youtube.com/watch?v=uvA3DUJCxpY">https://www.youtube.com/watch?v=uvA3DUJCxpY</a></li>
                 </ul>
             `;
         }
