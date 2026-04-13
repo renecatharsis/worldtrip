dc011531a2a26e6c31155e311d33547d07a96304
Rene Rösch <r.roesch90@gmail.com>
2026-04-13 23:32:05 +0200

    fix build (corepack duh)

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ docker run --rm \
   -v "$(pwd)":/opt/app \
   -w /opt/app \
   "$NODE" \
-  bash -c "pnpm install && pnpm run build && rm -rf node_modules"
+  bash -c "corepack enable && pnpm install && pnpm run build && rm -rf node_modules"
 
 echo "Cleaning up Docker image..."
 docker rmi -f "$NODE" || echo "Could not remove image $IMAGE (it may be in use or already removed)."
