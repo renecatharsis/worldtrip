e62de759ce75a94dfd0f39be3eb91ed3e92df152
Rene Rösch <r.roesch90@gmail.com>
2026-04-13 23:30:37 +0200

    switch to pnpm in build file, too

--- build.sh
+++ build.sh
@@ -3,18 +3,13 @@ set -e  # Exit immediately if a command exits with a non-zero status
 
 echo "Starting build process..."
 
-if ! command -v docker &> /dev/null; then
-  echo "Error: Docker is not installed or not in PATH."
-  exit 1
-fi
-
 NODE="node:24"
 
 docker run --rm \
   -v "$(pwd)":/opt/app \
   -w /opt/app \
   "$NODE" \
-  bash -c "npm install && npm run build && rm -rf node_modules"
+  bash -c "pnpm install && pnpm run build && rm -rf node_modules"
 
 echo "Cleaning up Docker image..."
 docker rmi -f "$NODE" || echo "Could not remove image $IMAGE (it may be in use or already removed)."
