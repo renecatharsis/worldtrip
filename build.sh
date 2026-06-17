36b5904ed71711e3f578e3ec1608a4fed20f4811
Rene Rösch <r.roesch90@gmail.com>
2026-06-17 21:37:36 +0200

    fix build (pinning to fixed version of node and pnpm)

--- build.sh
+++ build.sh
@@ -3,13 +3,13 @@ set -e  # Exit immediately if a command exits with a non-zero status
 
 echo "Starting build process..."
 
-NODE="node:24"
+NODE="node:24.16.0-alpine"
 
 docker run --rm \
   -v "$(pwd)":/opt/app \
   -w /opt/app \
   "$NODE" \
-  bash -c "corepack enable && pnpm install && pnpm run build && rm -rf node_modules"
+  bash -c "corepack prepare pnpm@10.33.4 --activate && corepack enable pnpm && pnpm install && pnpm run build && rm -rf node_modules"
 
 echo "Cleaning up Docker image..."
 docker rmi -f "$NODE" || echo "Could not remove image $IMAGE (it may be in use or already removed)."
